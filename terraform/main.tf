e482ff6682a8c600e415083529d634ddcaf70219
yawboateng104-lab <yaw.boateng104@gmail.com>
2026-05-13 03:04:48 -0400

    Format Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -245,10 +245,10 @@ resource "aws_db_instance" "mysql" {
 }
 
 resource "aws_instance" "mongo" {
-  ami                    = data.aws_ami.amazon_linux_2023.id
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public_a.id
-  key_name               = var.key_name
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_a.id
+  key_name      = var.key_name
   vpc_security_group_ids = [
     aws_security_group.management_sg.id,
     aws_security_group.database_sg.id
